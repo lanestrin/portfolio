3c9f2742d0174359b286928df084105e4269cf41
Lan Nguyen <Lan.Nguyen@unrivaledteamwear.com>
2026-07-22 11:05:44 -0500

    fix: resolve lint issue in app

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import "./styles/layout.scss";
 import "@fontsource-variable/roboto-condensed/wght.css";
 import "@fontsource-variable/inter/wght.css";
 import Header from "./components/Header/Header";
-import Certifications from "./components/Certification/Certification";
 import Education from "./components/Education/Education";
 import Experience from "./components/Experience/Experience";
 import Footer from "./components/Footer/Footer";
